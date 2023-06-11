1d6462f1b761a3f66927f2b37bdef8121b8823d0
Byron Tercero <B1tercero.cruz@gmail.com>
2023-06-11 16:32:47 -0600

    Create seccions html

--- js/app.js
+++ js/app.js
@@ -1,12 +1,10 @@
 const imagenes = document.querySelectorAll('.propiedad__imagen');
 
-
-
 window.addEventListener('scroll', () => {
 
     const scroll = this.scrollY / -20;
 
-    imagenes.forEach( (imagen) => {
+    imagenes.forEach((imagen) => {
         imagen.style.backgroundPositionY = `${scroll}px`;
     })
 });
