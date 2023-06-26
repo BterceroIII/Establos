09b0396394bc8d0a5d94ad4c72c047eb1dbf9f8d
Byron Tercero <B1tercero.cruz@gmail.com>
2023-06-26 14:29:03 -0600

    🔧 config gulp

--- gulpfile.js
+++ gulpfile.js
@@ -12,21 +12,31 @@ const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 const avif = require('gulp-avif');
 
-function css( done ) {
+// function css(done) {
+//     src('src/scss/app.scss')
+//         .pipe(sourcemaps.init())
+//         .pipe(sass())
+//         .pipe(postcss([autoprefixer(), cssnano()]))
+//         .pipe(sourcemaps.write('.'))
+//         .pipe(dest('build/css'))
+
+//     done();
+// }
+
+function css(done) {
     src('src/scss/app.scss')
-        .pipe( sourcemaps.init() )
-        .pipe( sass() )
-        .pipe( postcss([ autoprefixer(), cssnano() ]) )
-        .pipe( sourcemaps.write('.'))
-        .pipe( dest('build/css') )
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('build/css'));
 
     done();
 }
 
 function imagenes() {
     return src('src/img/**/*')
-        .pipe( imagemin({ optimizationLevel: 3 }) )
-        .pipe( dest('build/img') )
+        .pipe(imagemin({ optimizationLevel: 3 }))
+        .pipe(dest('build/img'))
 }
 
 function versionWebp() {
@@ -34,21 +44,21 @@ function versionWebp() {
         quality: 50
     }
     return src('src/img/**/*.{png,jpg}')
-        .pipe( webp( opciones ) )
-        .pipe( dest('build/img') )
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'))
 }
 function versionAvif() {
     const opciones = {
         quality: 50
     }
     return src('src/img/**/*.{png,jpg}')
-        .pipe( avif( opciones ) )
-        .pipe( dest('build/img'))
+        .pipe(avif(opciones))
+        .pipe(dest('build/img'))
 }
 
 function dev() {
-    watch( 'src/scss/**/*.scss', css );
-    watch( 'src/img/**/*', imagenes );
+    watch('src/scss/**/*.scss', css);
+    watch('src/img/**/*', imagenes);
 }
 
 
@@ -57,4 +67,4 @@ exports.dev = dev;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
 exports.versionAvif = versionAvif;
-exports.default = series( imagenes, versionWebp, versionAvif, css, dev  );
+exports.default = series(imagenes, versionWebp, versionAvif, css, dev);
